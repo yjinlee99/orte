316f6beeb1b367a7b0b9f01dedb894be18966f11
yjinlee99 <yjinlee99@gmail.com>
2026-09-10 17:22:32 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/com/orte/OrteApplication.java
@@ -0,0 +1,13 @@
+package com.orte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orte/OrteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
